a7580b8c7c706dc3efa48c9a0e5496c21e2ca8f8
raineser <105230611+raineser@users.noreply.github.com>
2023-12-08 12:13:08 -0600

    Update merkle.rs

--- src/merkle.rs
+++ src/merkle.rs
@@ -1,74 +1,79 @@
 #[derive(Debug)]
-pub struct data {
-    
-    name: String,
-    value: u128
-}
-
-impl data {
-    pub fn new(n: String, v: u128) -> data {
-        return data{name: n, value:v};
-    }
-    
-    
-    pub fn SetName(&mut self, newName: String) {
-        self.name = newName;
-    }
-    
-    pub fn SetValue(&mut self, newValue: u128) {
-        self.value = newValue;
-    }
-    
-    pub fn serial(&self) -> Vec<u8> {
-        let mut name_string = "name:".to_string();
-        name_string.push_str(&self.name);
-        
-        let mut value_string = ",value:".to_string();
-        value_string.push_str(&self.value.to_string());
-        
-        name_string.push_str(&value_string);
-      
-        let bytes = name_string.as_bytes().to_vec();
-
-        bytes
-        
-    }
-    
-    pub fn serialHash(&self) -> [u8;32] {
-        keccak256(&self.serial())
-    }
-}
-
-#[derive(Debug)]
 pub struct Merkle {}
 
 
 impl Merkle {
-
-    pub fn commit(leafs: Vec<[u8;32]> ) -> ([u8;32], Vec<[u8;32]>) {
+    
+    
+    pub fn commit(leafs: Vec<[u8;32]>) -> Vec<[u8;32]> {
         
         assert!(leafs.len() & (leafs.len() -1) == 0 );
         
-        let mut ileafs = leafs.clone();
+        if leafs.len() == 1 {
+            return [leafs[0]].to_vec()
+        } else {
+            
+            let first = leafs[leafs.len()/2..].to_vec();
+            let second = leafs[..leafs.len()/2].to_vec();
+            
+            //gross
+            let hash = keccak256(&[Merkle::commit(second), Merkle::commit(first)].concat()[..].concat()[..]).to_vec();
+            let result: Vec<[u8;32]> = hash.chunks_exact(32).map(|chunk| {
+                let mut array: [u8;32] = [0;32];
+                array.copy_from_slice(chunk);
+                array
+            }).collect();
+            result
+        }
+    }
+    
+     
+    pub fn open(index: usize, leafs: Vec<[u8;32]> ) -> Vec<[u8;32]> {
         
-        for branch in 0..leafs.len().ilog2() {
-            
-            let mut branch: Vec<[u8;32]> = vec![];
-            
-            for i in (0..ileafs.len()).step_by(2) {
-                
-                let mut a = [ileafs[i], ileafs[i+1]].concat();
-                
-                branch.push(keccak256(&a));
-                
-            }
-            
-            println!("{}", branch.len());
-            
-            ileafs = branch.clone();
+        assert!(leafs.len() & (leafs.len()-1) == 0);
+        assert!(0 <= index && index < leafs.len());
+        
+        if leafs.len() == 2 {
+            return [leafs[1-index]].to_vec();
         }
         
-        let root:[u8;32]  = ileafs[0];
-        return (root, leafs);
+        if index < leafs.len()/2 {
+            
+            let first = leafs[leafs.len()/2..].to_vec();
+            let second = leafs[..leafs.len()/2].to_vec();
+            
+            return [Merkle::open(index,second) , Merkle::commit(first)].concat();
+            
+        }else {
+            
+            let first = leafs[leafs.len()/2..].to_vec();
+            let second = leafs[..leafs.len()/2].to_vec();
+            
+            return [Merkle::open(index - leafs.len()/ 2 ,first) , Merkle::commit(second)].concat();
+        }
+    }
+    
+    pub fn verify (root: [u8;32], index: usize, path: Vec<[u8;32]>, leaf: [u8;32]) -> bool {
+        
+        assert!(0 <= index && index < (1 << path.len()));
+        
+        if path.len() == 1 {
+            if index == 0{
+
+                return root == keccak256( &[leaf, path[0]].concat()[..])
+            
+            } else {
+            
+                return root == keccak256( &[path[0], leaf].concat()[..])
+            }
+            
+        } else {
+            
+            if index % 2 == 0 {
+                return Merkle::verify(root, index >>1, path[1..].to_vec(), keccak256(&[leaf, path[0]].concat()[..]))
+            } else {
+                return Merkle::verify(root, index >>1, path[1..].to_vec(), keccak256(&[path[0], leaf].concat()[..]))
+            }
+        }
     }
 }
