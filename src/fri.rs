c7997d2eb7856a5ecf5adade4c343fc198cc27c1
raineser <105230611+raineser@users.noreply.github.com>
2023-12-12 09:46:10 -0600

    Update fri.rs

--- src/fri.rs
+++ src/fri.rs
@@ -1,4 +1,5 @@
-struct Fri {
+#[derive(Debug)]
+pub struct Fri {
     offset: FieldElement,
     omega: FieldElement, 
     domain_length : u128,
@@ -36,6 +37,50 @@ impl Fri {
             domain.push(self.offset * (self.omega ^ i));
         }
         domain
+    }
+    
+    pub fn commit(&self, codeword: &mut Vec<FieldElement>, proof_stream: &mut ProofStream, roundIndex: u128) -> Vec<FieldElement> {
+        
+        let one = FieldElement::one();
+        let two = FieldElement::new(2);
+        let mut omega = self.omega.clone();
+        let mut offset = self.offset.clone();
+        let mut codewords :Vec<FieldElement> = vec![];
+        
+        
+        for r in 0..self.num_rounds() {
+            
+            // compute and send Merkle root
+            let root = Merkle::commit(codeword);
+            proof_stream.push(&root);
+            
+            if r == self.num_rounds() - 1 {
+                break
+            }
+            
+            let alpha = FieldElement::sample(&proof_stream.prover_fiat_shamir(32));
+            
+            codewords.push(alpha.clone());
+            
+            let mut new_codeword:Vec<FieldElement> = vec![];
+            
+            for i in 0..codeword.len()/2 {
+                
+                new_codeword[i] = two.inverse().clone() * ((one.clone() + alpha.clone() / (offset.clone() * (omega.clone() ^ i as u128))) * codeword[i].clone() + (one.clone() - alpha.clone() / (offset.clone() * (omega.clone() ^ i as u128))) * codeword[codeword.len()/2 + i].clone());
+                
+            }
+            omega = omega ^ 2;
+            offset = offset ^ 2;
+            *codeword = new_codeword.clone();
+        }
+        
+        //proof_stream.push(codeword.clone());
+        
+        //codewords = [codewords, codeword].concat();
+        
+        
+        codewords
+    
     }
 
 }
